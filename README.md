8245b8bbf38b5027b21f5a58855f2e4212c2fa12
Samuel Fok <samuelfok2013@yahoo.com>
2024-04-28 13:32:34 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# twitter-search
+# Twitter Search App
+
+## Brief of technologies:
+
+### Server Backend:
+**ExpressJs** Web application framework to build web application on node.js (http://expressjs.com/)  
+**socket.io** To communicate in realtime between backend server and frontend without refreshing page (http://socket.io/)  
+**twitter** nodejs library to stream data from Twitter (https://www.npmjs.com/package/twitter)  
+**mongoose** Model nodejs library to work with MongoDB database (http://mongoosejs.com/)  
+
+### Database
+**MongoDB** data storage, install it on your machine(https://www.mongodb.org/).
+
+### Frontend:
+**angularjs** Javascript framework on frontend to build UI (https://angularjs.org/)
+**Bootstrap** Building website in responsive to have good looking on difference devices such as desktop browser, mobile browser, tablet browser....
+**jQuery** Fast, small, and feature-rich JavaScript library (https://jquery.com/)
+
+### Data model
+**models/tweet.js**
+You can change connection string there, default setup of mongodb is as current string(mongodb://localhost/tweets). If you have your own MongoDB opened port just change *localhost* to *localhost:PORT_NUMBER*
+
+### Basic logic of the app:
+Expressjs to run web application. Twitter nodejs library to stream data from Twitter into web application then use socket.io to stransfer data in realtime once backend receive data from Twitter stream.
+
+### To start app, issue command:
+node server.js
+
+### Edit if you want:
+To change port of the web application, edit line 33 *app.listen(8080);* :  
+In server.js file, replace 8080 to whatever you want.  
+You will browser the application on this port on your browser (eg, http://location:8080)  
+In models/tweet.js, replace *tweets* in mongodb://localhost/tweets if you want to change the database name where you want to store the data from Twitter
+
+To change port of socket to expose on server and listen on frontend:  
+* In server.js line 16 *server.listen('8000');* is the socket server port, change to whatever number you want.  
+* In public/index.html line 15 *<script src="//localhost:8000/socket.io/socket.io.js"></script>* change 8080 to the the same number has been changed above for the socket server port. Warning: This number required to have the same number with server socket port.
+
+To change Twitter authentication keys, edit server.js line 40:
+```javascript
+var clientTw = new Twitter({
+    consumer_key: 'v8YxY292pnVTZ7yXYRXMa2QE9',
+    consumer_secret: '0W5wJPrPwPaHCVjjBUiy6LCelIdmrt0XiM1pllufnYmMWVnelo',
+    access_token_key: '299634200-MKZTBg4aTJvdBxatAcbs34AgAovqlaRmaF31G4FF',
+    access_token_secret: 'v77XNxzxKUkWfgbUYeBD3s3Bvu8FyhxFd15Uup0siwHHk'
+});
+```
+Get start to create your own Twitter app and get authentication keys here https://apps.twitter.com/ . Create new app you you does not have one. Then get the keys from this app.
+
+**Note**
+Incase there is any error when start app with *node server.js*
+Delete **node_modules** folder and issue the following commands:
+npm install
+node server.js
+
+To start app again!
---
+++ server.js
@@ -0,0 +1,158 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var Twitter = require('twitter');
+
+//define express app to serve the site
+var app = express();
+
+//define socket
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+//require model
+var Tweets = require('./models/tweet.js').Tweets; 
+
+//put socket.io to listen to this port
+server.listen('8000');
+
+//serve static files from this folder
+app.use(express.static(__dirname + '/public'));
+
+//handler to parse data from body request
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+	extended: true
+}));
+
+//default route, index page
+app.get('/', function(req, res) {
+	res.redirect('/index.html');
+});
+
+//put app server to listen on port 8080. Browse http://localhost:8080 on your browser to start to use the app
+app.listen(8080);
+
+//listen to io connection from frontend
+io.on('connection', function(socket) {
+	//on searchSubmit request from frontend
+	socket.on('searchSubmit', function(data) {
+		//defined twitter api keys. Get it here https://apps.twitter.com/ .Create new app you you does not have one. Then get the keys from this app.
+		var clientTw = new Twitter({
+			consumer_key: 'v8YxY292pnVTZ7yXYRXMa2QE9',
+			consumer_secret: '0W5wJPrPwPaHCVjjBUiy6LCelIdmrt0XiM1pllufnYmMWVnelo',
+			access_token_key: '299634200-MKZTBg4aTJvdBxatAcbs34AgAovqlaRmaF31G4FF',
+			access_token_secret: 'v77XNxzxKUkWfgbUYeBD3s3Bvu8FyhxFd15Uup0siwHHk'
+		});
+
+		//default search in this location when no query input
+		var track = {
+			'locations': '-122.75,36.8,-121.75,37.8'
+		};
+		//for Suny Bay Lat & Long use '42.686496,-73.827203' instead of SF as above.
+
+		//incase there is search query string provided, then remove the location filter to use query string
+		if ('undefined' != typeof data.search_string && data.search_string.length > 0) {
+			delete track.locations; //remove default for location
+			track.track = data.search_string;
+		}
+
+		//default of total items to keep
+		var totalItem = 20 - 1;
+		if (!'undefined' != typeof data.total_item && data.total_item > 0) {
+			totalItem = data.total_item - 1;
+		}
+
+		//remove array items range
+		Array.prototype.remove = function(from, to) {
+			var rest = this.slice((to || from) + 1 || this.length);
+			this.length = from < 0 ? this.length + from : from;
+			return this.push.apply(this, rest);
+		};
+
+		var items = [];
+		var totalCount = 0; //count for total pushed intem
+
+		try {
+				//stream from twitter stream api
+				clientTw.stream('statuses/filter', track, function(stream) {
+					//on stream data
+					stream.on('data', function(tweet) {
+						//just steam with totalCount of item to be displayed on frontpage
+						if (totalCount <= totalItem) {
+							var dataLen = items.length;
+							/*if (dataLen > totalItem) {
+								//always keep the amount = totalItem only. If it is exceded then remove the old one.
+								items.remove(-totalItem)
+							}*/
+
+							if (dataLen <= totalItem) {
+								//define item data with specific fields
+								var itemToPush = {};
+								itemToPush.created_at = tweet.created_at;
+								itemToPush.id = tweet.id;
+								itemToPush.text = tweet.text;
+								itemToPush.user_id = tweet.user.id;
+								itemToPush.user_name = tweet.user.name;
+								itemToPush.user_screen_name = tweet.user.screen_name;
+								itemToPush.user_location = tweet.user.location;
+								itemToPush.user_followers_count = tweet.user.followers_count;
+								itemToPush.user_friends_count = tweet.user.friends_count;
+								itemToPush.user_created_at = tweet.user.created_at;
+								itemToPush.user_time_zone = tweet.user.time_zone;
+								itemToPush.user_profile_background_color = tweet.user.profile_background_color;
+								itemToPush.user_profile_image_url = tweet.user.profile_image_url;
+								itemToPush.geo = tweet.geo;
+								itemToPush.coordinates = tweet.coordinates;
+								itemToPush.place = tweet.place;
+
+								//push new data into array items list
+								items.push(itemToPush);
+
+
+					            ///create a new Tweet MongoDB document
+					            var tweet = new Tweets(itemToPush);
+					            //save into MongoDB Database
+					            tweet.save(function(err, data) {
+					                if (err) {
+					                    console.log('Erorr Saving the user data ' + err);
+					                } else {
+					                    console.log('User data Saved ' + data);
+					                }
+					            });
+
+								//increase count of totalCount
+								totalCount += 1;
+
+								//push data to frontend 
+								socket.emit('tweet', itemToPush);
+							}
+						}
+					});
+				});
+		} catch (err) {
+			//error log into console
+			console.log(err)
+		}
+	});
+
+	socket.on('searchdbSubmit',  function(data){
+		var searchString = data.search_string;
+		var totalItem = parseInt(data.total_item);
+
+		//define and execute query to fetch data from MongoDB
+        var query = Tweets.find({});
+        	if('undefined'!=typeof searchString && searchString.length>0){
+				query.where('text', new RegExp(searchString, 'i'));
+        	}
+			query.limit(totalItem);
+			query.sort({created_at: 1});
+
+			query.exec(function (err, data) {
+				if(err){
+					console.log(err);
+					return
+				}
+				socket.emit('tweetdb', data);
+			});
+	});
+});
